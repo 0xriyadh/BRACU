f75facd4c0ed0cd519f7856c4a7653de3c37f34f
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-06-22 18:48:54 +0600

    done with drawAxes()
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- CSE423/lab-01/lab-lecture/Lets_draw_sth.py
+++ CSE423/lab-01/lab-lecture/Lets_draw_sth.py
@@ -15,78 +15,81 @@ create_new = False
 
 class point:
     def __init__(self):
-        self.x=0
-        self.y=0
-        self.z=0
+        self.x = 0
+        self.y = 0
+        self.z = 0
 
 
 def crossProduct(a, b):
-    result=point()
+    result = point()
     result.x = a.y * b.z - a.z * b.y
     result.y = a.z * b.x - a.x * b.z
     result.z = a.x * b.y - a.y * b.x
 
     return result
 
-def convert_coordinate(x,y):
+
+def convert_coordinate(x, y):
     global W_Width, W_Height
     a = x - (W_Width/2)
-    b = (W_Height/2) - y 
-    return a,b
+    b = (W_Height/2) - y
+    return a, b
+
 
 def draw_points(x, y, s):
-    glPointSize(s) #pixel size. by default 1 thake
+    glPointSize(s)  # pixel size. by default 1 thake
     glBegin(GL_POINTS)
-    glVertex2f(x,y) #jekhane show korbe pixel
+    glVertex2f(x, y)  # jekhane show korbe pixel
     glEnd()
+
 
 def drawAxes():
     glLineWidth(1)
     glBegin(GL_LINES)
     glColor3f(1.0, 0.0, 0.0)
-    glVertex2f(250,0)
-    glVertex2f(-250,0)
+    glVertex2f(-250, 0)
+    glVertex2f(250, 0)
     glColor3f(0.0, 0.0, 1.0)
-    glVertex2f(0,250)
-    glVertex2f(0,-250)
+    glVertex2f(0, 250)
+    glVertex2f(0, -250)
     glEnd()
 
     glPointSize(5)
     glBegin(GL_POINTS)
     glColor3f(0, 1.0, 0.0)
-    glVertex2f(0,0)
- 
+    glVertex2f(0, 0)
+
     glEnd()
 
 
 def drawShapes():
     glBegin(GL_TRIANGLES)
-    glVertex2d(-170,170)
+    glVertex2d(-170, 170)
     glColor3f(0, 1.0, 0.0)
-    glVertex2d(-180,150)
-    glColor3f(1,0, 0.0)
-    glVertex2d(-160,150)
+    glVertex2d(-180, 150)
+    glColor3f(1, 0, 0.0)
+    glVertex2d(-160, 150)
     glEnd()
 
     glBegin(GL_QUADS)
-    glVertex2d(-170,120)
-    glColor3f(1,0, 1)
-    glVertex2d(-150,120)
-    glColor3f(0,0, 1)
-    glVertex2d(-150,140)
-    glColor3f(0,1,0)
-    glVertex2d(-170,140)
+    glVertex2d(-170, 120)
+    glColor3f(1, 0, 1)
+    glVertex2d(-150, 120)
+    glColor3f(0, 0, 1)
+    glVertex2d(-150, 140)
+    glColor3f(0, 1, 0)
+    glVertex2d(-170, 140)
     glEnd()
 
 
 def keyboardListener(key, x, y):
 
     global ball_size
-    if key==b'w':
-        ball_size+=1
+    if key == b'w':
+        ball_size += 1
         print("Size Increased")
-    if key==b's':
-        ball_size-=1
+    if key == b's':
+        ball_size -= 1
         print("Size Decreased")
     # if key==b's':
     #    print(3)
@@ -95,104 +98,114 @@ def keyboardListener(key, x, y):
 
     glutPostRedisplay()
 
+
 def specialKeyListener(key, x, y):
     global speed
-    if key=='w':
+    if key == 'w':
         print(1)
-    if key==GLUT_KEY_UP:
+    if key == GLUT_KEY_UP:
         speed *= 2
         print("Speed Increased")
-    if key== GLUT_KEY_DOWN:		#// up arrow key
+    if key == GLUT_KEY_DOWN:  #  up arrow key
         speed /= 2
         print("Speed Decreased")
     glutPostRedisplay()
     # if key==GLUT_KEY_RIGHT:
-        
+
     # if key==GLUT_KEY_LEFT:
-        
 
     # if key==GLUT_KEY_PAGE_UP:
-       
+
     # if key==GLUT_KEY_PAGE_DOWN:
-        
+
     # case GLUT_KEY_INSERT:
-    #   
+    #
     #
     # case GLUT_KEY_HOME:
-    #     
+    #
     # case GLUT_KEY_END:
-    #   
+    #
 
 
-def mouseListener(button, state, x, y):	#/#/x, y is the x-y of the screen (2D)
+def mouseListener(button, state, x, y):  # /#/x, y is the x-y of the screen (2D)
     global ballx, bally, create_new
-    if button==GLUT_LEFT_BUTTON:
-        if(state == GLUT_DOWN):    # 		// 2 times?? in ONE click? -- solution is checking DOWN or UP
-            print(x,y)
-            c_X, c_y = convert_coordinate(x,y)
+    if button == GLUT_LEFT_BUTTON:
+        if (state == GLUT_DOWN):  #  2 times?? in ONE click? -- solution is checking DOWN or UP
+            print(x, y)
+            c_X, c_y = convert_coordinate(x, y)
             ballx, bally = c_X, c_y
-        
-    if button==GLUT_RIGHT_BUTTON:
-        if state == GLUT_DOWN: 	
-            create_new = convert_coordinate(x,y)
+
+    if button == GLUT_RIGHT_BUTTON:
+        if state == GLUT_DOWN:
+            create_new = convert_coordinate(x, y)
     # case GLUT_MIDDLE_BUTTON:
-    #     //........
+    #     ........
 
     glutPostRedisplay()
 
 
 def display():
-    #//clear the display
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glClearColor(0,0,0,0);	#//color black
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    #//load the correct matrix -- MODEL-VIEW matrix
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # clear the display
+    glClearColor(0, 0, 0, 0)  # color black
+    glViewport(0, 0, W_Width, W_Height)
+
+    # load the correct matrix -- MODEL-VIEW matrix
     glMatrixMode(GL_MODELVIEW)
-    #//initialize the matrix
+    # initialize the matrix
     glLoadIdentity()
-    #//now give three info
-    #//1. where is the camera (viewer)?
-    #//2. where is the camera looking?
-    #//3. Which direction is the camera's UP direction?
-    gluLookAt(0,0,200,	0,0,0,	0,1,0)
+
+
+    """
+        gluLookAt function requires three info
+            1. where is the camera (viewer)? - Camera Position (0, 0, 200)
+            2. where is the camera looking? - Look At Point (0, 0, 0)
+            3. Which direction is the camera's UP direction? - Up Vector (0, 1, 0)
+        
+        1. Camera Position (0, 0, 200): This is where the camera is located in the 3D space. In your case, it's positioned at (0, 0, 200), which means it is 200 units away from the origin along the Z-axis. The camera is looking towards the origin if no other rotations are applied.
+
+        2. Look At Point (0, 0, 0): This is the point in the 3D space that the camera is looking at. Here, it's set to (0, 0, 0), the origin of the scene. This means the camera is directly looking towards the center of the scene.
+
+        3. Up Vector (0, 1, 0): This vector defines which direction is "up" from the perspective of the camera. In this case, (0, 1, 0) corresponds to the positive Y-axis being "up". This helps OpenGL determine the orientation of the camera. The up vector is perpendicular to the direction the camera is looking at.
+    """
+    gluLookAt(0, 0, 200,	0, 0, 0,	0, 1, 0)
     glMatrixMode(GL_MODELVIEW)
 
     drawAxes()
-    global ballx, bally, ball_size
-    draw_points(ballx, bally, ball_size)
-    drawShapes()
+    # global ballx, bally, ball_size
+    # draw_points(ballx, bally, ball_size)
+    # drawShapes()
 
-    glBegin(GL_LINES)
-    glVertex2d(180,0)
-    glVertex2d(180,180)
-    glVertex2d(180,180)
-    glVertex2d(0,180)
-    glEnd()
+    # glBegin(GL_LINES)
+    # glVertex2d(180, 0)
+    # glVertex2d(180, 180)
+    # glVertex2d(180, 180)
+    # glVertex2d(0, 180)
+    # glEnd()
 
-    if(create_new):
-        m,n = create_new
-        glBegin(GL_POINTS)
-        glColor3f(0.7, 0.8, 0.6)
-        glVertex2f(m,n)
-        glEnd()
-
+    # if (create_new):
+    #     m, n = create_new
+    #     glBegin(GL_POINTS)
+    #     glColor3f(0.7, 0.8, 0.6)
+    #     glVertex2f(m, n)
+    #     glEnd()
 
     glutSwapBuffers()
 
 
 def animate():
-    #//codes for any changes in Models, Camera
+    # codes for any changes in Models, Camera
     glutPostRedisplay()
-    global ballx, bally,speed
-    ballx=(ballx+speed)%180
-    bally=(bally+speed)%180
+    global ballx, bally, speed
+    ballx = (ballx+speed) % 180
+    bally = (bally+speed) % 180
+
 
 def init():
-    #//clear the screen
-    glClearColor(0,0,0,0)
-    #//load the PROJECTION matrix
+    # clear the screen
+    glClearColor(0, 0, 0, 0)
+    # load the PROJECTION matrix
     glMatrixMode(GL_PROJECTION)
-    #//initialize the matrix
+    # initialize the matrix
     glLoadIdentity()
 
     """  
@@ -229,11 +242,12 @@ glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB)
 wind = glutCreateWindow(b"OpenGL Coding Practice")
 init()
 
-glutDisplayFunc(display)	#display callback function
-# glutIdleFunc(animate)	#what you want to do in the idle time (when no drawing is occuring)
+glutDisplayFunc(display)  # display callback function
+# what you want to do in the idle time (when no drawing is occuring)
+glutIdleFunc(animate)
 
 glutKeyboardFunc(keyboardListener)
 glutSpecialFunc(specialKeyListener)
 glutMouseFunc(mouseListener)
 
-glutMainLoop()		#The main loop of OpenGL
+glutMainLoop()  # The main loop of OpenGL
