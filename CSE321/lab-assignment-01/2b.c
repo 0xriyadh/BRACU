9ebb4ff3b4cb785fc4beeed57f13629ad55e5da1
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-07-06 14:28:31 +0600

    good progress
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- CSE321/lab-assignment-01/2b.c
+++ CSE321/lab-assignment-01/2b.c
@@ -3,17 +3,17 @@
 int main()
 {   
     FILE *input_file, *output_file;
-    char input[100];
+    int lines = 0;
     
-    input_file = fopen("2b-input.txt", "w+");
-    fscanf(input_file, "%s", input);
+    input_file = fopen("2b-input.txt", "r");
+    
+    while (!feof(input_file)) {
+        char input[100];
+        fgets(input, sizeof(input), input_file);
+        lines++;
+        printf("line %d: %s", lines, input);
+    }
+    
     fclose(input_file);
-
-    printf("input: %s\n", input);
-
-    // output_file = fopen("2b-output.txt", "w+");
-    // fprintf(output_file, "%s", input);
-    // fclose(output_file);
-
     return 0;
 }
