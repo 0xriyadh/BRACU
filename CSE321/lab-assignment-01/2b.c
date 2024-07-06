152d3cf225d9241c65cd26c39eef6e66f7f3e938
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-07-06 15:33:44 +0600

    2b done
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- CSE321/lab-assignment-01/2b.c
+++ CSE321/lab-assignment-01/2b.c
@@ -7,14 +7,12 @@ int main()
     char input[100];
     char divider[] = " ";
     char *token;
-    int lines = 0;
 
     input_file = fopen("2b-input.txt", "r");
     output_file = fopen("2b-output.txt", "w");
 
     while (fgets(input, sizeof(input), input_file) != NULL)
     {
-        lines++;
         char newline[100] = "";
 
         // getting the first token
@@ -31,7 +29,7 @@ int main()
                 strcat(newline, token);
             }
         }
-        fprintf(output_file, "line %d: %s", lines, newline);
+        fprintf(output_file, "%s", newline);
     }
 
     fclose(input_file);
