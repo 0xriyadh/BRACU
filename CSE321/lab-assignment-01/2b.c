e05a4918cbdd3369b686b393317b765cd4374be6
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-07-06 14:32:20 +0600

    wow, c is amazing
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- CSE321/lab-assignment-01/2b.c
+++ CSE321/lab-assignment-01/2b.c
@@ -3,17 +3,17 @@
 int main()
 {   
     FILE *input_file, *output_file;
+    char input[100];
     int lines = 0;
     
     input_file = fopen("2b-input.txt", "r");
-    
-    while (!feof(input_file)) {
-        char input[100];
-        fgets(input, sizeof(input), input_file);
+
+    while (fgets(input, sizeof(input), input_file) != NULL)
+    {
         lines++;
         printf("line %d: %s", lines, input);
     }
-    
+
     fclose(input_file);
     return 0;
 }
