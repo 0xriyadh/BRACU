b157f1e38d79b22f5aa7d18a64c82e9e038ccff5
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-07-06 15:28:32 +0600

    wow done with this, after a lot of try
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- CSE321/lab-assignment-01/2b.c
+++ CSE321/lab-assignment-01/2b.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
-{   
+{
     FILE *input_file, *output_file;
     char input[100];
+    char divider[] = " ";
+    char *token;
     int lines = 0;
-    
+
     input_file = fopen("2b-input.txt", "r");
+    output_file = fopen("2b-output.txt", "w");
 
     while (fgets(input, sizeof(input), input_file) != NULL)
     {
         lines++;
-        printf("line %d: %s", lines, input);
+        char newline[100] = "";
+
+        // getting the first token
+        token = strtok(input, divider);
+        strcat(newline, token);
+
+        // getting the rest of the tokens
+        while (token != NULL)
+        {
+            token = strtok(NULL, divider);
+            if (token != NULL)
+            {
+                strcat(newline, " ");
+                strcat(newline, token);
+            }
+        }
+        fprintf(output_file, "line %d: %s", lines, newline);
     }
 
     fclose(input_file);
