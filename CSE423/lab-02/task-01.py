be4922979ca2464c47bd86016238d862d5788ad7
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-09-03 11:27:28 +0600

    basic game done

--- CSE423/lab-02/task-01.py
+++ CSE423/lab-02/task-01.py
@@ -2,6 +2,7 @@ import random
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import sys
 
 W_Width, W_Height = 500, 500
 pause = False
@@ -9,6 +10,9 @@ gameOver = False
 showDiamond = False
 diamondInfo = None
 speed = 0.4
+catcher_position = 200
+catcher_pause = False
+score = 0
 
 def drawPixel(x, y):
     glPointSize(2)
@@ -108,10 +112,10 @@ def drawFinalLine(x0, y0, x1, y1, zone):
             y += 1
 
 def drawDiamond(x, y):
-    drawLine(x, y, x - 12, y - 20)
-    drawLine(x, y, x + 12, y - 20)
-    drawLine(x, y - 40, x - 12, y - 20)
-    drawLine(x, y - 40, x + 12, y - 20)
+    drawLine(x, y, x - 10, y - 20)
+    drawLine(x, y, x + 10, y - 20)
+    drawLine(x, y - 40, x - 10, y - 20)
+    drawLine(x, y - 40, x + 10, y - 20)
 
 def generateDiamond():
     global diamondInfo
@@ -119,7 +123,6 @@ def generateDiamond():
     y = 440
     r, g, b = random.uniform(0.5, 1.0), random.uniform(0.5, 1.0), random.uniform(0.5, 1.0)
     
-    print("Diamond Generated at", x, y, "with color", r, g, b)
     diamondInfo = [x, y, r, g, b]
 
 def drawCatcher(x):
@@ -136,50 +139,63 @@ def drawCatcher(x):
     drawLine(x + 100, y, x + 80, y - 15)
     drawLine(x + 20, y - 15, x + 80, y - 15)   
 
-def keyboardListener(key, x, y):
-    if key == b'd':
-        if day_light[0] >= 1 and day_light[1] >= 1 and day_light[2] >= 1:
-            print("Day Mode Activated")
-            return
-        day_light[0] += 0.2
-        day_light[1] += 0.2
-        day_light[2] += 0.2
-        opposite_day_light[0] -= 0.2
-        opposite_day_light[1] -= 0.2
-        opposite_day_light[2] -= 0.2
-        print("Day Light Increased", day_light)
-    if key == b'n':
-        if day_light[0] <= 0 and day_light[1] <= 0 and day_light[2] <= 0:
-            print("Night Mode Activated")
-            return
-        day_light[0] -= 0.2
-        day_light[1] -= 0.2
-        day_light[2] -= 0.2
-        opposite_day_light[0] += 0.2
-        opposite_day_light[1] += 0.2
-        opposite_day_light[2] += 0.2
-        print("Day Light Decreased")
+# AABB Collision Detection algorithm
+def hasCollided(x1, y1, x2, y2):
+    if x1 < x2 + 100 and x1 + 100 > x2 and y1 < (y2 + 15) and (y1 + 40) > y2:
+        return True
+    return False
 
+def mouseListener(button, state, x, y):
+    global speed, pause, gameOver, showDiamond, diamondInfo, score, catcher_position, catcher_pause
+
+    if button==GLUT_LEFT_BUTTON:
+        if state==GLUT_DOWN:
+
+            # close button
+            if 450<x<490 and 450< (W_Height - y) <490: #converting y coordinate
+                print(f"Goodbye! Score: {score}")
+                sys.exit(0)
+
+            # pause button
+            elif 230<x<270 and 450<(500-y)<490:
+                print("Paused/Resumed")
+                if pause == False:
+                    pause = True
+                    catcher_pause = True
+                else:
+                    pause = False
+                    catcher_pause = False
+                
+            #restart
+            elif 5<x<45 and 455<(500-y)<495:
+                print("Starting over!")
+                showDiamond=False
+                diamondInfo=None
+                score=0
+                catcher_position=200
     glutPostRedisplay()
 
-def specialKeyListener(key, x, y):
-    global rain_direction
-    if key == GLUT_KEY_LEFT:
-        rain_direction -= 5
-        print("Rain Direction Changing towards Left")
-    if key == GLUT_KEY_RIGHT:  # up arrow key
-        rain_direction += 5
-        print("Rain Direction Changing towards Right")
-
+def specialKeyListener(key, a, b):
+    global catcher_position, gameOver, catcher_pause
+    if not gameOver and not catcher_pause:
+        if key==GLUT_KEY_RIGHT:
+            if catcher_position < 400:
+                catcher_position+=15
+        if key==GLUT_KEY_LEFT:
+            if catcher_position > 0:
+                catcher_position-=15
     glutPostRedisplay()
 
 def animate():
-    global showDiamond, speed, diamondInfo
+    global showDiamond, speed, diamondInfo, pause
     if showDiamond:
         x, y, r, g, b = diamondInfo
         glColor3f(r, g, b)
         drawDiamond(x, y)
-        diamondInfo[1] = diamondInfo[1] - speed
+        
+        if not pause:
+            diamondInfo[1] = diamondInfo[1] - speed
+
     glutPostRedisplay() 
 
 def display():
@@ -209,15 +225,36 @@ def display():
     drawLine(450, 490, 490, 450)
     drawLine(450, 450, 490, 490)
 
+    global gameOver, diamondInfo, showDiamond, score, catcher_position, speed, score
+
     if not gameOver:
-        global diamondInfo, showDiamond
-        
         if not showDiamond:
-            print("Generating Diamond")
             generateDiamond()
             showDiamond = True
+        else:
+            x1 = diamondInfo[0]
+            y1 = diamondInfo[1]
+
+            x2 = catcher_position
+            y2 = 15
+
+            # if hasCollided(x1, y1, x2, y2):
+            if (y1-40) <= 15 and x2 <= x1 <= x2+100: 
+                score += 1
+                showDiamond = False
+                diamondInfo = None
+                print("Score:", score)
+                speed *= 1.2
+            elif y1 <= y2:
+                gameOver = True
+                print(f"Game over! Score: {score}")
+                score = 0
+                showDiamond = False
+                diamondInfo = None
+                speed = 0.4
 
     animate()
+    drawCatcher(catcher_position)
     glutSwapBuffers()
 
 def init():
@@ -237,16 +274,6 @@ glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB)
 wind = glutCreateWindow(b"Catch The Diamond")
 init()
 glutDisplayFunc(display)
-
-# for i in range(300):
-#     x = random.randint(0, W_Width)
-#     y = random.randint(0, W_Height)
-#     rain_drops.append([x, y])
-# glutIdleFunc(animate)
-# print(rain_drops)
-
-# # glutMouseFunc(mouseListener)
-# glutKeyboardFunc(keyboardListener)
-# glutSpecialFunc(specialKeyListener)
-
-glutMainLoop()  # The main loop of OpenGL
+glutSpecialFunc(specialKeyListener)
+glutMouseFunc(mouseListener)
+glutMainLoop()
