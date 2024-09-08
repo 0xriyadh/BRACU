b95d6de9d02739c895f0f0cb8ae1d9cb9b55a478
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-09-08 12:10:12 +0600

    system calls task 04 oddeven.c done
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>

---
+++ CSE321/lab-assignment-02/task-04-oddeven.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <numbers>\n", argv[0]);
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        int num = atoi(argv[i]);
+        if (num % 2 == 0)
+        {
+            printf("%d is even\n", num);
+        }
+        else
+        {
+            printf("%d is odd\n", num);
+        }
+    }
+
+    return 0;
+}
