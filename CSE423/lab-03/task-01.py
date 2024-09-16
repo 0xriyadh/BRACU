c2dd3464687adf4cc02023866ed5ca5f1d71a7b8
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-09-16 21:17:04 +0600

    going in the right direction

---
+++ CSE423/lab-03/task-01.py
@@ -0,0 +1,308 @@
+import random
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+W_Width, W_Height = 500, 500
+pause = False
+gameOver = False
+showDiamond = False
+diamondInfo = None
+speed = 0.4
+catcher_position = 200
+catcher_pause = False
+catcher_speed = 15
+score = 0
+circles = []
+bullets = []
+circle_miss_count = 0
+bullet_miss_count = 0
+shooter_position = 250
+
+def drawPixel(x, y):
+    glPointSize(2)
+    glBegin(GL_POINTS)
+    glVertex2f(x, y)
+    glEnd()
+
+def zone_identifier(dx, dy):
+    if dx >= 0:
+        if dy >= 0:
+            if abs(dx) >= abs(dy):
+                return 0
+            else:
+                return 1
+        else:
+            if abs(dx) >= abs(dy):
+                return 7
+            else:
+                return 6
+    else:
+        if dy >= 0:
+            if abs(dx) >= abs(dy):
+                return 3
+            else:
+                return 2
+        else:
+            if abs(dx) >= abs(dy):
+                return 4
+            else:
+                return 5
+
+# convert from zone 0 to other zones
+def zone_converter_0_to_others(x, y, zone):
+    if zone == 0:
+        return x, y
+    elif zone == 1:
+        return y, x
+    elif zone == 2:
+        return -y, x
+    elif zone == 3:
+        return -x, y
+    elif zone == 4:
+        return -x, -y
+    elif zone == 5:
+        return -y, -x
+    elif zone == 6:
+        return y, -x
+    elif zone == 7:
+        return x, -y
+    
+# convert from other zones to zone 0
+def zone_converter_other_to_0(x, y, zone):
+    if zone == 0:
+        return x, y
+    elif zone == 1:
+        return y, x
+    elif zone == 2:
+        return y, -x
+    elif zone == 3:
+        return -x, y
+    elif zone == 4:
+        return -x, -y
+    elif zone == 5:
+        return -y, -x
+    elif zone == 6:
+        return -y, x
+    elif zone == 7:
+        return x, -y
+  
+def drawLine(x0, y0, x1, y1):
+    dx = x1 - x0
+    dy = y1 - y0
+
+    zone = zone_identifier(dx, dy) 
+
+    x0, y0 = zone_converter_other_to_0(x0, y0, zone)
+    x1, y1 = zone_converter_other_to_0(x1, y1, zone)
+
+    drawFinalLine(x0, y0, x1, y1, zone)
+
+def drawFinalLine(x0, y0, x1, y1, zone):
+    dx = x1-x0
+    dy = y1-y0
+    dE = 2*(dy)
+    dNE = 2*(dy - dx)
+    d = 2*(dy) - dx
+    x=x0
+    y=y0
+    while x < x1:
+        drawPixel(*zone_converter_0_to_others(x, y, zone))
+        if d < 0:
+            d += dE
+            x += 1
+        else:
+            d += dNE
+            x += 1
+            y += 1
+
+def drawCircle(x0, y0, radius):
+    x = radius
+    y = 0
+    d = 1 - radius
+
+    while x >= y:
+        # Draw 8 octants of the circle
+        drawPixel(x0 + x, y0 + y)
+        drawPixel(x0 - x, y0 + y)
+        drawPixel(x0 + x, y0 - y)
+        drawPixel(x0 - x, y0 - y)
+        drawPixel(x0 + y, y0 + x)
+        drawPixel(x0 - y, y0 + x)
+        drawPixel(x0 + y, y0 - x)
+        drawPixel(x0 - y, y0 - x)
+
+        y += 1
+        if d <= 0:
+            d += 2 * y + 1
+        else:
+            x -= 1
+            d += 2 * (y - x) + 1
+
+def shooter(x):
+    global gameOver
+    if not gameOver:
+        glColor3f(1, 1, 1)
+    else:
+        glColor3f(1, 0, 0)
+    drawCircle(x, 30, 20)
+
+# AABB Collision Detection algorithm
+def hasCollided(x1, y1, x2, y2):
+    if x1 < x2 + 100 and x1 + 100 > x2 and y1 < (y2 + 15) and (y1 + 40) > y2:
+        return True
+    return False
+
+def mouseListener(button, state, x, y):
+    global speed, pause, gameOver, showDiamond, diamondInfo, score, catcher_position, catcher_pause
+
+    if button==GLUT_LEFT_BUTTON:
+        if state==GLUT_DOWN:
+
+            # close button
+            if 450<x<490 and 450< (W_Height - y) <490: #converting y coordinate
+                print(f"Goodbye! Score: {score}")
+                glutDestroyWindow(glutGetWindow())  # Close the window
+
+            # pause button
+            elif 230<x<270 and 450<(500-y)<490:
+                print("Paused/Resumed")
+                if pause == False:
+                    pause = True
+                    catcher_pause = True
+                else:
+                    pause = False
+                    catcher_pause = False
+                
+            #restart
+            elif 5<x<45 and 455<(500-y)<495:
+                print("Starting over!")
+                showDiamond=False
+                diamondInfo=None
+                gameOver=False
+                score=0
+                catcher_position=200
+    glutPostRedisplay()
+
+def specialKeyListener(key, a, b): 
+    glutPostRedisplay()
+
+def animate():
+    global gameOver, circles, circle_miss_count, bullets, bullet_miss_count, speed, score
+
+    if not gameOver:
+        if len(bullets) > 0:
+            i = 0
+            while i < (len(bullets)):
+                r, x, y = bullets[i]
+                glColor3f(0.8, 0.3, 1)
+                drawCircle(x, y, r)
+                if pause == False:
+                    bullets[i][1] += 3
+                if bullets[i][1] > 500:
+                    bullets.pop(i)
+                    bullet_miss_count += 1
+                    print(f"Misfired {bullet_miss_count}/3")
+                    if bullet_miss_count == 3:
+                        gameOver = True
+                        print(f"Game Over! Your Score: {score}")
+                        break
+                    i -= 1       
+                i += 1                 
+
+        if len(circles) > 0:
+            i = 0
+            while i < len(circles):
+                x, y, r = circles[i]
+                glColor3f(0.8, 1, 0.3)
+                drawCircle(x, y, r)
+                circles[i][1] -= speed
+                if circles[i][1] < 0:
+                    circles.pop(i)
+                    circle_miss_count += 1
+                    print(f"Circle Missed {circle_miss_count}/3")
+                    if circle_miss_count == 3:
+                        gameOver = True
+                        print(f"Game Over! Your Score: {score}")
+                        break
+                    i -= 1
+                i += 1
+
+    glutPostRedisplay() 
+
+def display():
+    global shooter_position, gameOver, pause
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # clear the display
+    glClearColor(0, 0, 0, 0)
+    
+    # drawCircle(50, 50, 10)
+
+    # restart button
+    glColor3f(0.2,0.8,0.9)
+    drawLine(10, 470, 50, 470)
+    drawLine(10, 470, 30, 490)
+    drawLine(10, 470, 30, 450)
+
+    # pause button
+    if pause == False:
+        glColor3f(1.0,0.8,0.2)
+        drawLine(240, 490, 240, 450)
+        drawLine(260, 490, 260, 450)
+    else:
+        glColor3f(1.0, 0.8, 0.2)
+        drawLine(230, 490, 270, 470)
+        drawLine(230, 490, 230, 450)
+        drawLine(230, 450, 270, 470)
+
+    # close button
+    glColor3f(1.0,0.0,0.0)
+    drawLine(450, 490, 490, 450)
+    drawLine(450, 450, 490, 490)
+
+    if not gameOver:
+        if len(circles) == 0:
+            circles.append([random.randint(31, 470), 500, 30])
+        elif circles[-1][1] < 400:
+            circles.append([random.randint(31, 470), 500, 30])
+
+        vanishing_circles = []
+        vanishing_bullets = []
+
+        for i in range(len(circles)):
+            x_circle, y_circle, r_circle = circles[i]
+            for j in range(len(bullets)):
+                r_bullet, x_bullet, y_bullet = bullets[j]
+                if (x_bullet - x_circle)**2 + (y_bullet - y_circle)**2 <= (r_bullet + r_circle)**2:
+                    vanishing_circles.append(i)
+                    vanishing_bullets.append(j)
+                    score += 1
+                    print(f"Score: {score}")
+                    break
+
+
+
+    shooter(shooter_position)
+    animate()
+    glutSwapBuffers()
+
+def init():
+    # clear the screen
+    glClearColor(0, 0, 0, 0)
+    # # load the PROJECTION matrix 
+    # glMatrixMode(GL_PROJECTION)
+    # # initialize the matrix
+    # glLoadIdentity()
+    # gluPerspective(104,	1,	1,	1000.0)
+    glOrtho(0, 500, 0, 500, 0, 1)
+
+glutInit()
+glutInitWindowSize(W_Width, W_Height)
+glutInitWindowPosition(0, 0)
+glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB)
+wind = glutCreateWindow(b"Shoot The Circles")
+init()
+glutDisplayFunc(display)
+glutSpecialFunc(specialKeyListener)
+glutMouseFunc(mouseListener)
+glutMainLoop()
